acdc2047ec58f6fbc9c50de05c017e4fa95b7571
John <johnruge@uchicago.edu>
2024-07-23 09:12:41 +0300

    chore: adding data

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import argparse
+from fastapi import FastAPI, HTTPException
 # from dataclasses import dataclass
 from langchain_community.vectorstores import Chroma
 from langchain_openai import OpenAIEmbeddings
@@ -19,14 +19,16 @@ fact that you are Bishara Plus AI assistant everytime you answer the question).
 Answer the question based on the above context: {question}
 """
 
+app = FastAPI()
 
-def main():
-    # Create CLI.
-    parser = argparse.ArgumentParser()
-    parser.add_argument("query_text", type=str, help="The query text.")
-    args = parser.parse_args()
-    query_text = args.query_text
+@app.get("/")
+def root():
+    return {"Biashara Plus": "AI assistant"}
 
+@app.get("/{query_text}")
+def answer_question(query_text: str) -> str:
+    # if query_text == "":
+    #     raise HTTPException(status_code=400, detail="Query can't be an empty string")
     # Prepare the DB.
     embedding_function = OpenAIEmbeddings()
     db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
@@ -34,21 +36,17 @@ def main():
     # Search the DB.
     results = db.similarity_search_with_relevance_scores(query_text, k=3)
     if len(results) == 0 or results[0][1] < 0.7:
-        print(f"Thank you for reaching out! I'm sorry, but I don't have the information you need right now. You might want to contact this number [+255 679 516 178] for further assistance.")
-        return
+        return "Thank you for reaching out! I'm sorry, but I don't have the information" \
+               " you need right now. You might want to contact this number [+255 679 516 178]" \
+               " for further assistance."
 
     context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
     prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
     prompt = prompt_template.format(context=context_text, question=query_text)
-    print(prompt)
 
     model = ChatOpenAI()
     response_text = model.predict(prompt)
 
-    sources = [doc.metadata.get("source", None) for doc, _score in results]
-    formatted_response = f"Response: {response_text}\nSources: {sources}"
-    print(formatted_response)
-
-
-if __name__ == "__main__":
-    main()
+    # sources = [doc.metadata.get("source", None) for doc, _score in results]
+    # response_with_sources = f"Response: {response_text}\nSources: {sources}"
+    return response_text
